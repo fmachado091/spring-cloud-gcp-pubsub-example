4256326b58049a0cd47257d055839f00c3a6b458
fmachado091 <fmachado091@gmail.com>
2018-12-02 20:07:56 -0200

    Create project

---
+++ src/main/java/br/com/quintoandar/springcloudgcppubsubexample/SpringCloudGcpPubSubExampleApplication.java
@@ -0,0 +1,12 @@
+package br.com.quintoandar.springcloudgcppubsubexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGcpPubSubExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGcpPubSubExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/quintoandar/springcloudgcppubsubexample/SpringCloudGcpPubSubExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.quintoandar.springcloudgcppubsubexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudGcpPubSubExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
