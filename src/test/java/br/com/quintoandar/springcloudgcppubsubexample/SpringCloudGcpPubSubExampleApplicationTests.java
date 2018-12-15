00d5d0e2af3799a3ac36e0aec186294cd3ae820b
fmachado091 <fmachado091@gmail.com>
2018-12-15 16:57:07 -0200

    Fix tabs

--- src/test/java/br/com/quintoandar/springcloudgcppubsubexample/SpringCloudGcpPubSubExampleApplicationTests.java
+++ src/test/java/br/com/quintoandar/springcloudgcppubsubexample/SpringCloudGcpPubSubExampleApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringCloudGcpPubSubExampleApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
