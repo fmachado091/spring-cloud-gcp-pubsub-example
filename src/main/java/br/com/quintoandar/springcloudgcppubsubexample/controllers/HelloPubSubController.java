f4aaddee7b9112153746a0b5ca97a9b60c903546
fmachado091 <fmachado091@gmail.com>
2018-12-20 23:30:26 -0200

    Create HelloPubSubController

---
+++ src/main/java/br/com/quintoandar/springcloudgcppubsubexample/controllers/HelloPubSubController.java
@@ -0,0 +1,31 @@
+package br.com.quintoandar.springcloudgcppubsubexample.controllers;
+
+import br.com.quintoandar.springcloudgcppubsubexample.publishers.HelloPubSubPublisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloPubSubController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloPubSubController.class);
+
+    private final HelloPubSubPublisher publisher;
+
+    @Autowired
+    public HelloPubSubController(HelloPubSubPublisher publisher) {
+        this.publisher = publisher;
+    }
+
+    @PostMapping("/publish")
+    public void publish(@RequestBody String message) {
+        LOGGER.info("Received a POST at /hello/publish with message=[{}]", message);
+        publisher.publish(message);
+    }
+
+}
